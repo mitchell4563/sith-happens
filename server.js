0cfea0c9f1a5d728581760a18f4053f9746cd813
Toliver Crisp <92277632+tolivercrisp@users.noreply.github.com>
2022-12-20 22:02:28 -0500

    Initial auth attempts

--- server.js
+++ server.js
@@ -1,17 +1,42 @@
-// import Express
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers/api');
+
+
+const sequelize = require('./config/connection');
+
+// Create a new sequelize store using the express-session package
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Middleware for parsing JSON and urlencoded form data
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
+// const hbs = exphbs.create({ helpers });
 
-// Sends 'Hello' World to index.html
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// Configure and link a session object with the sequelize store
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+// Add express-session and store as Express.js middleware
+app.use(session(sess));
+
+// app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
 
 // Server is listening
 app.listen(PORT, () => {
---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+// copied
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
+  
