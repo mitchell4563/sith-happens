6de8d2fe05d12fb015e8c339adb7bd9263cb3974
Toliver Crisp <92277632+tolivercrisp@users.noreply.github.com>
2022-12-22 14:53:18 -0500

    Okay NOW the login process works, using a session with cookies

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers/api');
+const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 
-// TODO: Add a comment describing the functionality of this expression
-// Creating a sequelize store by importing the express session package
+// Create a new sequelize store using the express-session package
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -16,20 +15,18 @@ const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
-// TODO: Add a comment describing the functionality of this object
-// Establishes a new session object
+// Configure and link a session object with the sequelize store
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
-// TODO: Add a comment describing the functionality of this statement
-// Specifying elements of the current session
+// Add express-session and store as Express.js middleware
 app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
