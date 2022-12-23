08a1dc75de2fe880b4364c04085e3e778e43488f
Toliver Crisp <92277632+tolivercrisp@users.noreply.github.com>
2022-12-22 19:31:55 -0500

    Added yoda to our server listening line cause why not

--- server.js
+++ server.js
@@ -39,5 +39,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(
+    `
+    ==============================
+          "Online, Server is."  
+                __.-._                   
+                '-._"7'  
+                 /'.-c          
+                 |  /T   
+                _)_/LI     
+    ==============================
+
+    `));
 });
