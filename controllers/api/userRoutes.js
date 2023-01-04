181e6a0da2121694958d3cb097d6a9a0ec259d0a
b0yage <christivnvertiz@gmail.com>
2023-01-03 19:39:49 -0600

    sign up sheet done

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,27 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
+//sign up information
+router.post('/', async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 router.post('/login', async (req, res) => {
   try {
     // Find the user who matches the posted e-mail address
@@ -35,15 +56,6 @@ router.post('/login', async (req, res) => {
     res.status(400).json(err);
   }
 });
-
-router.post('/signup', async (req, res) => {
-  try {
-
-  }
-})
-
-
-
 
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
