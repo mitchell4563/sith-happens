f287f1bd55374c2f36a130ee7c22a41fd1aeb905
Mitchell Wood <mitchellwood11@gmail.com>
2023-01-05 18:56:34 -0600

    corrected bounties and get route for users

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const { response } = require('express');
 const { User } = require('../../models');
 
-
+router.get('/', async (req, res) => {
+  try {
+    const userData = await User.findAll()
+    res.status(200).json(userData)
+  } catch (error){
+    res.status(500).json(error)
+  }
+})
 //sign up information
 router.post('/', async (req, res) => {
   try {
